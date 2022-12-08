ef3919102f7750a62d7ffe4e81954a035998e76b
Pasha <nikolenko.pavel@gmail.com>
2022-12-08 09:11:42 +0300

    Изначальный вариант.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void task1() {
+
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+}
